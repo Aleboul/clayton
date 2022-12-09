16913223eb728462bf3e2403920ec0b1f17ca1bb
Alexis Boulin <82594658+Aleboul@users.noreply.github.com>
2022-12-09 08:38:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=open('README.md', 'r').read(),
     author = 'Alexis Boulin',
     author_email = 'aboulin@unice.fr',
-    url = 'https://github.com/Aleboul/computo',
+    url = 'https://github.com/Aleboul/clayton',
     download_url = 'https://github.com/Aleboul/clayton/',
     classifiers = [],
     include_package_data=True,
